99cbf2aa54272bc862bc48018bd4314a24f7c7a6
omid-joukar <joukaromid6@gmail.com>
2021-03-18 15:59:59 +0100

    added payment POJO. Closes #87

---
+++ src/main/java/antigypt/springframework/domain/Payment.java
@@ -0,0 +1,42 @@
+package antigypt.springframework.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * Created by omid on 11/18/2020.
+ */
+@Entity
+@Table(name = "payment")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentsId;
+    @Column(name = "amount")
+    private Double amount;
+    @Column(name = "paymentDate")
+    private LocalDate paymentDate;
+    @Column(name = "paymentTime")
+    private LocalTime paymentTime;
+    @Column(name = "rebate")
+    private Double rebate;
+
+    @Enumerated
+    private PaymentType paymentType;
+
+    @OneToOne(mappedBy = "payment")
+    @JoinColumn(name = "orderId")
+    private Order order;
+
+
+}
---
+++ src/main/java/antigypt/springframework/domain/PaymentType.java
@@ -0,0 +1,13 @@
+package antigypt.springframework.domain;
+
+/**
+ * Created by omid on 11/18/2020.
+ */
+public enum PaymentType {
+    CASH,
+    CHECKS,
+    DEBIT_CARD,
+    CREDIT_CARD,
+    MOBILE_PAYMENT,
+    ELECTRONIC_BANK_TRANSFERS
+}
