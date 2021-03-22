d78c936c3d6e015e881930eebd122ed964ccdde1
omid-joukar <joukaromid6@gmail.com>
2021-03-22 16:02:32 +0100

    added order DTO. Closes #92

---
+++ src/main/java/antigypt/springframework/api/v1/model/OrderDTO.java
@@ -0,0 +1,28 @@
+package antigypt.springframework.api.v1.model;
+
+import antigypt.springframework.domain.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDTO {
+
+
+    private String orderDate;
+    private String orderTime;
+    private String requiredDate;
+    private String shippedDate;
+    private String shippedTime;
+    private String comments;
+    private OrderStatus orderStatus;
+    private DepartmentDTO department;
+    private EmployeeDTO deliverEmployee;
+    private CustomerDTO customer;
+    private String OrderUrl;
+}
