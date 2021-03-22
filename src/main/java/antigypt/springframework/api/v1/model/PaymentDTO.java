d64f0f96fe93a821fb09b2c7b98c2a2bdf88e7aa
omid-joukar <joukaromid6@gmail.com>
2021-03-22 16:01:33 +0100

    added payment DTO. Closes #93

---
+++ src/main/java/antigypt/springframework/api/v1/model/PaymentDTO.java
@@ -0,0 +1,28 @@
+package antigypt.springframework.api.v1.model;
+
+import antigypt.springframework.domain.Order;
+import antigypt.springframework.domain.PaymentType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDTO {
+
+    
+    private Double amount;
+    private LocalDate paymentDate;
+    private LocalTime paymentTime;
+    private Double rebate;
+    private PaymentType paymentType;
+    private OrderDTO order;
+    private String paymentUrl;
+}
