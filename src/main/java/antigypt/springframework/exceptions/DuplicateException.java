dbe67fcfe6e895af998bdc8f67b6c66de2e9a19b
omid-joukar <joukaromid6@gmail.com>
2021-04-05 22:32:51 +0200

    added duplicate exception. Closes #132

---
+++ src/main/java/antigypt/springframework/exceptions/DuplicateException.java
@@ -0,0 +1,15 @@
+package antigypt.springframework.exceptions;
+
+public class DuplicateException extends Exception{
+    public DuplicateException() {
+        super();
+    }
+
+    public DuplicateException(String message) {
+        super(message);
+    }
+
+    public DuplicateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
