39196578e3f6256ca133d2dc96a0bbbd614f9d5e
omid-joukar <joukaromid6@gmail.com>
2021-04-05 22:33:27 +0200

    added exception handler. Closes #133

--- src/main/java/antigypt/springframework/controllers/api/v1/ExceptionHandlerController.java
+++ src/main/java/antigypt/springframework/controllers/api/v1/ExceptionHandlerController.java
@@ -1,21 +1,27 @@
 package antigypt.springframework.controllers.api.v1;
 
+import antigypt.springframework.exceptions.DuplicateException;
 import antigypt.springframework.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
 @ControllerAdvice
-public class ExceptionHandlerController{
+public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleResourceNotFoundException(Exception exception, Model model){
-        model.addAttribute("exception" , exception);
-        return "error";
+    public ResponseEntity<Object> handleResourceNotFoundException(Exception exception, WebRequest webRequest){
+        String errorMessage = "The object doesnt exists";
+        return new ResponseEntity<>(errorMessage,HttpStatus.NOT_FOUND);
+    }
 
+    @ExceptionHandler(DuplicateException.class)
+    public ResponseEntity<Object> handleDuplicateException(Exception exception, WebRequest webRequest){
+        String errorMessage = "the object already exists";
+        return new ResponseEntity<>(errorMessage,HttpStatus.BAD_REQUEST);
     }
 }
