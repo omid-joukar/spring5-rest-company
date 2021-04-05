fe2b12c86f2601f9d99795becf1365c7b8e55f86
omid-joukar <joukaromid6@gmail.com>
2021-04-05 22:28:24 +0200

    added subscribe DTO. Closes #127

---
+++ src/main/java/antigypt/springframework/api/v1/model/SubscribeDTO.java
@@ -0,0 +1,15 @@
+package antigypt.springframework.api.v1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscribeDTO {
+    private String email;
+    private String subscribeUrl;
+}
