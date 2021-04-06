5bbee5b369b50ea2198a402db543ed32b18c0c77
omid-joukar <joukaromid6@gmail.com>
2021-04-06 21:57:27 +0200

    do some refactoring. Closes #141

--- src/main/resources/spring-rest-company/src/components/Home/slider.js
+++ src/main/resources/spring-rest-company/src/components/Home/slider.js
@@ -2,30 +2,33 @@ import React, { useState , useEffect } from 'react';
 import Slider from 'react-slick';
 import axios from 'axios';
 import {URL_SLIDES} from '../utils/pathes';
-const settings = {
-    arrows: true,
-    dots: false,
-    infinite: false,
-    speed:500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-}
 
 
-const HomeSlider = () =>{
+
+const HomeSlider = (props) =>{
+
+    const settings = {
+        arrows: true,
+        dots: false,
+        infinite: false,
+        speed:500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
+
+
     let [slides,setSlides] = useState([]);
     useEffect( () => {
         const fetchSlides = async ()=> {
            try{
                const response = await axios.get(URL_SLIDES);
-               setSlides(response.data)
+               setSlides(response.data.slides)
            } catch(error){
                 console.log(error);
            }
         }
         fetchSlides();
     }, [])
-console.log(slides);
     return(
         <>
         
@@ -34,10 +37,10 @@ console.log(slides);
             <Slider {...settings}>
                 {
                 slides.map( item =>(
-                    <div className="slide-container pl-0 col-sm-12" key={item.id}>
+                    <div className="slide-container pl-0 col-sm-12" key={item.slideUrl}>
                         <div className= "item_slider"
                         style={{
-                           background: `url(/images/slider/${item.cover}) no-repeat` 
+                           background: `url(/images/slider/${item.cover}) no-repeat`
                         }}>
                             
                         </div>
