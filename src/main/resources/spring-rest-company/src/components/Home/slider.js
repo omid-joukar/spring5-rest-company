610be41fadc7dfc2a36d91d2b160ba2d01bc9594
omid-joukar <joukaromid6@gmail.com>
2021-04-09 18:24:00 +0200

    added topic and title to sliders. Closes #145

--- src/main/resources/spring-rest-company/src/components/Home/slider.js
+++ src/main/resources/spring-rest-company/src/components/Home/slider.js
@@ -8,9 +8,9 @@ import {URL_SLIDES} from '../utils/pathes';
 const HomeSlider = (props) =>{
 
     const settings = {
-        arrows: true,
+        arrows: false,
         dots: false,
-        infinite: false,
+        infinite: true,
         speed:500,
         slidesToShow: 1,
         slidesToScroll: 1
@@ -37,12 +37,15 @@ const HomeSlider = (props) =>{
             <Slider {...settings}>
                 {
                 slides.map( item =>(
-                    <div className="slide-container pl-0 col-sm-12" key={item.slideUrl}>
+                    <div className="slide-container  p-0 m-0 col-sm-12" key={item.slideUrl}>
                         <div className= "item_slider"
                         style={{
                            background: `url(/images/slider/${item.cover}) no-repeat`
                         }}>
-                            
+                           <div className="caption">
+                               <h4>{item.topic}</h4>
+                               <p>{item.title}</p>
+                           </div>
                         </div>
                     </div>
                 ))
