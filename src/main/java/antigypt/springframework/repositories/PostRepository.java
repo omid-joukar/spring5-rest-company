8a956c5b93e02f19f7dc88289ed7e1dfc042be26
omid-joukar <joukaromid6@gmail.com>
2021-04-16 16:33:59 +0200

    added post repository. Closes #148

---
+++ src/main/java/antigypt/springframework/repositories/PostRepository.java
@@ -0,0 +1,7 @@
+package antigypt.springframework.repositories;
+
+import antigypt.springframework.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post,Long> {
+}
