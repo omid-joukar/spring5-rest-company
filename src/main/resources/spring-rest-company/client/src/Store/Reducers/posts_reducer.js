7955816e14007c9cb71d80215e741e233953c557
omid-joukar <joukaromid6@gmail.com>
2021-04-25 19:19:38 +0200

    added post reducer. Closes #179

--- src/main/resources/spring-rest-company/client/src/Store/Reducers/posts_reducer.js
+++ src/main/resources/spring-rest-company/client/src/Store/Reducers/posts_reducer.js
@@ -1,6 +1,9 @@
+import {POSTS} from '../types';
 export default function(state={},action){
+
     switch(action.type){
-        
+        case POSTS:
+            return {...state,posts:action.payload.posts}
         default:
             return state;
     }
