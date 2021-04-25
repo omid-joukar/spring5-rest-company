dd2fffe134f8355e172af86f8cc5cca887582428
omid-joukar <joukaromid6@gmail.com>
2021-04-25 19:23:28 +0200

    added sale reducer. Closes #182

--- src/main/resources/spring-rest-company/client/src/Store/Reducers/sales_reducer.js
+++ src/main/resources/spring-rest-company/client/src/Store/Reducers/sales_reducer.js
@@ -1,6 +1,8 @@
+import {SALES} from '../types';
 export default function(state={},action){
     switch(action.type){
-        
+        case SALES:
+            return {...state,sales:action.payload.sales}
         default:
             return state;
     }
