682c010d969d1d28692f40553bf2dbd3c61ee54f
omid-joukar <joukaromid6@gmail.com>
2021-05-01 11:11:33 +0200

    added customer component . closes #206

---
+++ src/main/resources/spring-rest-company/client/src/components/User/customer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Customer extends Component {
+    render() {
+        return (
+            <div>
+                Hello Masoud!!
+            </div>
+        )
+    }
+}
+export default Customer;
